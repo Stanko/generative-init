70d4fdebb1b55970c5c5a9879a7c18f17cf7c056
Stanko <stanko.tadic@gmail.com>
2024-10-18 20:57:39 +0200

    Updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default {
   server: {
     port: 1234,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // or "modern"
+      },
+    },
+  },
   root: './src',
 };
