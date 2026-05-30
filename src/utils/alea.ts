133e8359ee338be392e5776f0f1e94988c97166b
Stanko <stanko.tadic@gmail.com>
2026-05-30 11:16:23 +0200

    Minor tweaks

--- src/utils/alea.ts
+++ src/utils/alea.ts
@@ -2,43 +2,52 @@
 // Slim TypeScript version of this implementation:
 // https://github.com/coverslide/node-alea
 
-const getMash = () => {
-  let n = 0xefc8249d;
+const Alea = (...seeds: string[]): (() => number) => {
+  const getMash = () => {
+    let n = 0xefc8249d;
 
-  const mash = (seed: string): number => {
-    for (let i = 0; i < seed.length; i++) {
-      n += seed.charCodeAt(i);
-      let h = 0.02519603282416938 * n;
-      n = h >>> 0;
-      h -= n;
-      h *= n;
-      n = h >>> 0;
-      h -= n;
-      n += h * 0x100000000; // 2^32
-    }
-    return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+    const mash = (seed: string): number => {
+      for (let i = 0; i < seed.length; i++) {
+        n += seed.charCodeAt(i);
+        let h = 0.02519603282416938 * n;
+        n = h >>> 0;
+        h -= n;
+        h *= n;
+        n = h >>> 0;
+        h -= n;
+        n += h * 0x100000000; // 2^32
+      }
+      return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+    };
+
+    return mash;
   };
 
-  return mash;
-};
+  const getRandomSeeds = () => {
+    return [
+      Math.random().toString(36).slice(2),
+      Math.random().toString(36).slice(2),
+      Math.random().toString(36).slice(2),
+    ];
+  };
 
-const Alea = (seed: string = Date.now().toString()): (() => number) => {
-  let s = [0, 0, 0];
+  const mash = getMash();
+  const s = [mash(' '), mash(' '), mash(' ')];
   let c = 1;
 
-  let mash = getMash();
+  (seeds || getRandomSeeds()).forEach((seed) => {
+    s.forEach((_, i) => {
+      s[i] -= mash(seed);
 
-  s.forEach((_, i) => {
-    s[i] = mash(' ') - mash(seed);
-
-    if (s[i] < 0) {
-      s[i] += 1;
-    }
+      if (s[i] < 0) {
+        s[i] += 1;
+      }
+    });
   });
 
   const random = () => {
     const t = 2091639 * s[0] + c * 2.3283064365386963e-10; // 2^-32
-    c = t | 0;
+    c = t | 0; // quicker floor
     s[0] = s[1];
     s[1] = s[2];
     s[2] = t - c;
